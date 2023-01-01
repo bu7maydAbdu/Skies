e0d1815b9fac6b1d5941b351af65b41c3a27c378
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2023-01-02 01:16:40 +0300

    screen diplay on click of info button

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,24 @@
-require('dotenv').config({path: '../config/.env'})
+// require('dotenv').config({path: '../config/.env'})
 
 
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-.then((response) => response.json())
-.then((data) => {
-    console.log(data)
-    const img = document.querySelector("img")
-    img
-})
-.catch(err => {
-    console.log(err)
-})
+// fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data)
+//     const img = document.querySelector("img")
+//     img
+// })
+// .catch(err => {
+//     console.log(err)
+// })
+
+const infoBtn = document.querySelector(".info-btn")
+
+infoBtn.addEventListener("click", showScreen)
+
+
+function showScreen (){
+    const explanationScreen = document.querySelector(".explanation-screen")
+    explanationScreen.classList.remove("none")
+}
